4771f6ccfa3ded1969ef39c6b87310148eee05fb
Osama Mosaad <osama.mosaad@basharsoft.com>
2022-08-14 05:34:55 +0200

    feat: make a symfony command line `app:log-importer`

---
+++ src/Command/LogImporterCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Command;
+
+use App\Services\LogAnalyzer\Application\Command\ImportLog;
+
+use Symfony\Component\Console\{
+    Command\Command,
+    Attribute\AsCommand,
+    Input\InputArgument,
+    Input\InputInterface,
+    Output\OutputInterface,
+};
+
+#[AsCommand(
+    name: 'app:log-importer',
+    description: 'To import log file',
+)]
+class LogImporterCommand extends Command
+{
+    public function __construct(
+        private ImportLog $importService,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setHelp(
+            "The app:log-importer command import and stores log data into DB.
+            bin/console app:log-importer <file path>"
+        )
+            ->addArgument("file", InputArgument::REQUIRED, "Pass file path");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $filePath = $input->getArgument('file');
+        $output->writeln(sprintf('Start to Import, %s', $filePath));
+
+        $this->importService->exec($filePath);
+        $output->writeln('Done');
+
+        return Command::SUCCESS;
+    }
+}
